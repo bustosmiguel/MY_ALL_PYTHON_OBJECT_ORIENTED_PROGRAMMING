c7b6c52a873839a9edbf36165b0542dc39f1d360
REGRESSIONDATA <miguelbustossaez@gmail.com>
2026-02-20 15:54:59 -0300

    first commit

---
+++ Clases y Objetos.py
@@ -0,0 +1,26 @@
+#%%Clases y objetos
+
+class Celular():
+    marca = "SAMSUNG"
+    modelo = "S23"
+    camara = "48MP"
+
+celular1 = Celular()
+celular2 = Celular()
+celular3 = Celular()
+celular4 = Celular()
+print(celular2.modelo)
+
+#%%Atributos de instancia (o propiedades de instancia)
+class Celular():
+    marca = "SAMSUNG"
+    modelo = "S23"
+    camara = "48MP"
+
+celular1 = Celular()
+celular2 = Celular()
+celular3 = Celular()
+celular4 = Celular()
+print(celular2.camara)
+
+# %%
